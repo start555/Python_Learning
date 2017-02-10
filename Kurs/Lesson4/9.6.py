8172ab9f4a308dd29a0ffaafc777059b0619e46c
start555 <e.ripinsky@gmail.com>
2017-02-10 18:48:00 +0300

    Update 9.6.py

--- Kurs/Lesson4/9.6.py
+++ Kurs/Lesson4/9.6.py
@@ -4,6 +4,8 @@
 # и протестировать эту функцию на вычислении длины окружности и площади круга,
 # а также сравнить вычисленное значение числа ПИ с math.pi из стандартной библиотеки.
 
+import math
+
 def func(n):
     m = 1
     znak =1
@@ -14,5 +16,11 @@ def func(n):
         znak *= -1
     return (my_pi*4)
 
-
-print(func(800000))
+r_kruga = 40
+l1_kruga = str(func(800000)*2*r_kruga)
+l2_kruga = str(math.pi*2*r_kruga)
+print('L круга (ПИ из func): ' + l1_kruga)
+print('L круга (ПИ из math.pi): '+ l2_kruga)
+print('S круга (ПИ из func): ' + str(func(800000)*(r_kruga**2)))
+print('S круга (ПИ из math.pi): ' + str(math.pi*(r_kruga**2)))
+print('Дельта между ПИ:' + str(math.pi - func(800000)))
